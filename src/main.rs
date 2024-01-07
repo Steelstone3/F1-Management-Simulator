b2251aea8aff878296700d98a022c2fb6b344a32
Skippy <steelstone3@gmail.com>
2024-01-07 23:49:08 +0000

    update

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
-mod models;
+use controller::random_generator::generate_seed;
+use models::driver::Driver;
+
 mod controller;
+mod models;
 
 fn main() {
-
+    let _driver = Driver::new([
+        generate_seed(),
+        generate_seed(),
+        generate_seed(),
+        generate_seed(),
+        generate_seed(),
+    ]);
 }
--- src/models/driver.rs
+++ src/models/driver.rs
@@ -1,3 +1,5 @@
+use rand::random;
+
 use super::{driver_name::DriverName, driver_statistics::DriverStatistic};
 
 pub struct Driver {
@@ -6,7 +8,36 @@ pub struct Driver {
 }
 
 impl Driver {
-    fn new() {
-        
+    pub fn new(seeds: [u64; 5]) -> Self {
+        Self {
+            name: random(),
+            statistics: DriverStatistic::new(seeds),
+        }
+    }
+}
+
+#[cfg(test)]
+mod driver_should {
+    use super::*;
+
+    #[test]
+    fn new_driver() {
+        // When
+        let driver = Driver::new([1, 2, 3, 4, 5]);
+
+        // Then
+        assert_ne!("".to_string(), driver.name.to_string());
+        assert_ne!(100, driver.statistics.awareness);
+        assert_ne!(49, driver.statistics.awareness);
+        assert_ne!(100, driver.statistics.consistency);
+        assert_ne!(49, driver.statistics.consistency);
+        assert_ne!(100, driver.statistics.experience);
+        assert_ne!(49, driver.statistics.experience);
+        assert_ne!(100, driver.statistics.race_craft);
+        assert_ne!(49, driver.statistics.race_craft);
+        assert_ne!(100, driver.statistics.pace);
+        assert_ne!(49, driver.statistics.pace);
+        assert_ne!(100, driver.statistics.overall);
+        assert_ne!(49, driver.statistics.overall);
     }
 }
