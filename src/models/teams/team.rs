24a814031a5f9092ac9183f0bf1cbc8d16027668
Skippy <steelstone3@gmail.com>
2024-01-09 02:13:27 +0000

    update

--- src/models/teams/team.rs
+++ src/models/teams/team.rs
@@ -1,5 +1,6 @@
-use crate::models::{car::Car, drivers::driver::Driver};
 use super::{team_name::TeamName, team_statistics::TeamStatistic};
+use crate::models::{car::Car, drivers::driver::Driver};
+use rand::random;
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct Team {
@@ -7,6 +8,22 @@ pub struct Team {
     pub statistics: TeamStatistic,
     pub car: Car,
     pub drivers: [Driver; 2],
+}
+
+impl Team {
+    pub fn new(
+        team_seeds: [u64; 5],
+        car_seeds: [u64; 4],
+        driver_1_seeds: [u64; 5],
+        driver_2_seeds: [u64; 5],
+    ) -> Self {
+        Self {
+            name: random(),
+            statistics: TeamStatistic::new(team_seeds),
+            car: Car::new(car_seeds),
+            drivers: [Driver::new(driver_1_seeds), Driver::new(driver_2_seeds)],
+        }
+    }
 }
 
 #[cfg(test)]
