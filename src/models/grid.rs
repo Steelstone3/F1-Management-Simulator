ff2a1ed4b85a93fb7ef10f2d513b65d496b9f60e
Skippy <steelstone3@gmail.com>
2024-01-09 03:08:53 +0000

    update

--- src/models/grid.rs
+++ src/models/grid.rs
@@ -4,3 +4,24 @@ pub struct Grid {
     teams: [Team; 10],
 }
 
+impl Grid {
+    pub fn new() -> Self {
+        Self {
+            teams: [
+                Team::new(),
+                Team::new(),
+                Team::new(),
+                Team::new(),
+                Team::new(),
+                Team::new(),
+                Team::new(),
+                Team::new(),
+                Team::new(),
+                Team::new(),
+            ],
+        }
+    }
+}
+
+#[cfg(test)]
+mod grid_should {}
