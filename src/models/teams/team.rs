53b82634d9f2d5efaf52d3c557c438da5a7bdebb
Skippy <steelstone3@gmail.com>
2024-01-09 01:42:58 +0000

    Update team.rs

--- src/models/teams/team.rs
+++ src/models/teams/team.rs
@@ -1,9 +1,10 @@
 use crate::models::{car::Car, drivers::driver::Driver};
-use super::team_name::TeamName;
+use super::{team_name::TeamName, team_statistics::TeamStatistic};
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct Team {
     pub name: TeamName,
+    pub statistics: TeamStatistic,
     pub car: Car,
     pub drivers: [Driver; 2],
 }
