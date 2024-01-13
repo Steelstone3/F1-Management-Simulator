d959c5ae103f8995f8d14af2ff54741ced8ee114
Skippy <steelstone3@gmail.com>
2024-01-13 16:32:18 +0000

    update

--- src/models/teams/team_name.rs
+++ src/models/teams/team_name.rs
@@ -1,7 +1,7 @@
 use rand_derive2::RandGen;
 use std::fmt::{Display, Formatter};
 
-#[derive(RandGen, Debug, PartialEq, Eq)]
+#[derive(RandGen, Clone, Copy, Debug, PartialEq, Eq)]
 pub enum TeamName {
     Ferrari,
     Mercedes,
