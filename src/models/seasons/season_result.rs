2dbaeaf709a2efe2f0e64ffa9b1575cadb71f6ef
Skippy <steelstone3@gmail.com>
2024-01-15 04:38:57 +0000

    Update season_result.rs

--- src/models/seasons/season_result.rs
+++ src/models/seasons/season_result.rs
@@ -1,3 +1,4 @@
+use std::fmt::Display;
 use crate::models::{races::race_grid::TEAMS_ON_THE_RACE_GRID, teams::team::Team};
 
 // TODO aggrigate result holder
@@ -5,8 +6,18 @@ pub struct SeasonResult {
     teams: [Team; TEAMS_ON_THE_RACE_GRID],
 }
 
+impl Display for SeasonResult {
+    fn fmt(&self, formatting: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(formatting, "Something")
+    }
+}
+
 impl SeasonResult {
     pub fn new(teams: [Team; TEAMS_ON_THE_RACE_GRID]) -> Self {
         Self { teams }
+    }
+
+    pub fn aggregate_driver_points() {
+
     }
 }
