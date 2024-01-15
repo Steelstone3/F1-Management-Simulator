30831487f20094f97794a8ae4921ee118e8f48ee
Skippy <steelstone3@gmail.com>
2024-01-15 01:47:27 +0000

    update

--- src/models/drivers/driver.rs
+++ src/models/drivers/driver.rs
@@ -1,12 +1,13 @@
 use super::{driver_name::DriverName, driver_statistics::DriverStatistic};
 use crate::models::{
     points::Points,
-    teams::{team::Team, team_name::TeamName}, races::race_grid::TEAMS_ON_THE_RACE_GRID,
+    races::race_grid::TEAMS_ON_THE_RACE_GRID,
+    teams::{team::Team, team_name::TeamName},
 };
+use core::panic;
 use std::fmt::Display;
 
-#[derive(Clone, Copy)]
-#[derive(Debug, Default, PartialEq, Eq)]
+#[derive(Clone, Copy, Debug, Default, PartialEq, Eq)]
 pub struct Driver {
     pub driver_name: DriverName,
     pub team_name: TeamName,
@@ -30,30 +31,41 @@ impl Driver {
         }
     }
 
-    pub fn find_team(&self, _teams: &[Team; TEAMS_ON_THE_RACE_GRID]) -> usize {
-        // TODO
-        // Finds the team for points allocation
-        todo!()
+    pub fn find_team(&self, teams: &[Team; TEAMS_ON_THE_RACE_GRID]) -> usize {
+        let mut team_index = 0;
+
+        for team in teams {
+            if self.team_name == team.team_name {
+                return team_index;
+            }
+            team_index += 1;
+        }
+
+        panic!("No team found for driver");
     }
 }
 
-// TODO change driver display to show driver name, team name and points
 impl Display for Driver {
     fn fmt(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             formatter,
             "Driver: {}\nTeam: {}\nDriver Points: {}\n",
-            self.driver_name, self.team_name, self.driver_points.calculate_season_points()
+            self.driver_name,
+            self.team_name,
+            self.driver_points.calculate_season_points()
         )
     }
 }
 
 #[cfg(test)]
 mod driver_should {
+    use rstest::rstest;
+
     use crate::models::{
         drivers::{driver::Driver, driver_name::DriverName, driver_statistics::DriverStatistic},
         points::Points,
-        teams::team_name::TeamName,
+        races::race_grid::TEAMS_ON_THE_RACE_GRID,
+        teams::{team::Team, team_name::TeamName},
     };
 
     #[test]
@@ -89,7 +101,8 @@ mod driver_should {
     #[test]
     fn display_driver() {
         // Given
-        let expected_driver_display = "Driver: Lewis Hamilton\nTeam: Mercedes\nDriver Points: 0\n".to_string();
+        let expected_driver_display =
+            "Driver: Lewis Hamilton\nTeam: Mercedes\nDriver Points: 0\n".to_string();
         let driver = Driver {
             driver_name: DriverName::LewisHamilton,
 
@@ -111,5 +124,65 @@ mod driver_should {
 
         // Then
         assert_eq!(expected_driver_display, driver_display);
+    }
+
+    #[rstest]
+    #[case(2, TeamName::Mercedes)]
+    #[case(7, TeamName::AlphaTauri)]
+    #[case(0, TeamName::RedBull)]
+    fn find_team_associated_with_driver(#[case]expected_team_index:usize, #[case]team_name:TeamName) {
+        // Given
+        let driver = Driver {
+            team_name,
+            ..Default::default()
+        };
+        let teams: [Team; TEAMS_ON_THE_RACE_GRID] = [
+            Team {
+                team_name: TeamName::RedBull,
+                ..Default::default()
+            },
+            Team {
+                team_name: TeamName::Ferrari,
+                ..Default::default()
+            },
+            Team {
+                team_name: TeamName::Mercedes,
+                ..Default::default()
+            },
+            Team {
+                team_name: TeamName::Mclaren,
+                ..Default::default()
+            },
+            Team {
+                team_name: TeamName::AstonMartin,
+                ..Default::default()
+            },
+            Team {
+                team_name: TeamName::Alpine,
+                ..Default::default()
+            },
+            Team {
+                team_name: TeamName::Williams,
+                ..Default::default()
+            },
+            Team {
+                team_name: TeamName::AlphaTauri,
+                ..Default::default()
+            },
+            Team {
+                team_name: TeamName::AlphaRomeo,
+                ..Default::default()
+            },
+            Team {
+                team_name: TeamName::Haas,
+                ..Default::default()
+            },
+        ];
+
+        // When
+        let team_index=driver.find_team(&teams);
+
+        // Then
+        assert_eq!(expected_team_index, team_index);
     }
 }
