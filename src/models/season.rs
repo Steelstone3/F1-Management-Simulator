4f519840af9417b4dd6328ae57380a8bf153bc5e
Skippy <steelstone3@gmail.com>
2024-01-25 00:50:56 +0000

    Update season.rs

--- src/models/season.rs
+++ src/models/season.rs
@@ -1,6 +1,10 @@
-pub const NUMBER_OF_RACES: usize = 10;
+use super::team::Team;
 
+pub const NUMBER_OF_RACES: usize = 10;
+pub const NUMBER_OF_TEAMS: usize = 10;
+
+#[allow(dead_code)]
 #[derive(Copy,Clone)]
 pub struct Season {
-    
+    teams: [Team; NUMBER_OF_TEAMS]
 }
